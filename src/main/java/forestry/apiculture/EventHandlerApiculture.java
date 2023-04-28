7cd22241e70814b1fd4e323a84a7f616a7b36b6d
DStrand1 <dane@strandboge.com>
2023-04-28 02:19:55 -0500

    Auto-apply stats tooltips to all IHiveFrame items

---
+++ src/main/java/forestry/apiculture/EventHandlerApiculture.java
@@ -0,0 +1,85 @@
+package forestry.apiculture;
+
+import java.util.List;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import forestry.api.apiculture.IBeeModifier;
+import forestry.api.apiculture.IHiveFrame;
+import forestry.core.proxy.Proxies;
+import forestry.core.utils.StringUtil;
+
+public class EventHandlerApiculture {
+
+    @SubscribeEvent
+    public void addFrameTooltip(ItemTooltipEvent event) {
+        ItemStack itemStack = event.itemStack;
+        if (itemStack != null && itemStack.getItem() instanceof IHiveFrame) {
+            IHiveFrame frame = (IHiveFrame) itemStack.getItem();
+            IBeeModifier modifier = frame.getBeeModifier();
+            List<String> tooltip = event.toolTip;
+
+            if (!Proxies.common.isShiftDown()) {
+                tooltip.add(EnumChatFormatting.ITALIC + "<" + StringUtil.localize("gui.tooltip.tmi") + ">");
+            } else {
+                int durability = itemStack.getMaxDamage();
+                tooltip.add(StringUtil.localize("frame.tooltip.durability") + getDurabilityFormatted(durability));
+
+                float territory = modifier.getTerritoryModifier(null, 1.0F);
+                tooltip.add(StringUtil.localize("frame.tooltip.territory") + getColorFormatted(territory));
+
+                float mutation = modifier.getMutationModifier(null, null, 1.0F);
+                tooltip.add(StringUtil.localize("frame.tooltip.mutationRate") + getColorFormatted(mutation));
+
+                float lifespan = modifier.getLifespanModifier(null, null, 1.0F);
+                tooltip.add(StringUtil.localize("frame.tooltip.lifespan") + getColorFormatted(lifespan));
+
+                float production = modifier.getProductionModifier(null, 1.0F);
+                tooltip.add(StringUtil.localize("frame.tooltip.production") + getColorFormatted(production));
+
+                float decay = modifier.getGeneticDecay(null, 1.0F);
+                tooltip.add(StringUtil.localize("frame.tooltip.geneticDecay") + getColorFormatted(decay));
+            }
+        }
+    }
+
+    private static String getDurabilityFormatted(int durability) {
+        EnumChatFormatting color;
+        if (durability < 100) {
+            color = EnumChatFormatting.DARK_RED;
+        } else if (durability < 200) {
+            color = EnumChatFormatting.RED;
+        } else if (durability < 250) {
+            color = EnumChatFormatting.GOLD;
+        } else if (durability < 750) {
+            color = EnumChatFormatting.GREEN;
+        } else {
+            color = EnumChatFormatting.AQUA;
+        }
+        return color + " " + durability;
+    }
+
+    private static String getColorFormatted(float value) {
+        EnumChatFormatting color;
+        if (value <= 0.5F) {
+            color = EnumChatFormatting.DARK_RED; // "bad" stat
+        } else if (value < 1.0F) {
+            color = EnumChatFormatting.RED; // "below average" stat
+        } else if (value == 1.0F) {
+            color = EnumChatFormatting.GOLD; // "average" stat
+        } else if (value <= 2.0F) {
+            color = EnumChatFormatting.GREEN; // "above average" stat
+        } else {
+            color = EnumChatFormatting.AQUA; // "great" stat
+        }
+
+        // trim trailing zeroes if no decimal-precision is needed
+        if (value == (long) value) {
+            return color + " x" + (long) value;
+        }
+        return color + " x" + value;
+    }
+}
